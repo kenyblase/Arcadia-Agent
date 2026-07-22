d6eae5ac3fbcd82e6e041094d9490e4cd6f58d8e
kenyblase <kenyblase@gmail.com>
2026-07-22 18:46:46 +0200

    update

--- api/apiClient.ts
+++ api/apiClient.ts
@@ -1,9 +1,9 @@
-import { useAuthStore } from "@/lib/store/authStore";
-import { useRouter } from "next/router";
+// import { useAuthStore } from "@/lib/store/authStore";
+// import { useRouter } from "next/router";
 import axios from "axios";
 
-const { logout } = useAuthStore();
-const router = useRouter()
+// const { logout } = useAuthStore();
+// const router = useRouter()
 
 const api = axios.create({
   baseURL: process.env.NEXT_PUBLIC_API_URL,
@@ -25,16 +25,16 @@ api.interceptors.request.use((config) => {
   return config;
 });
 
-api.interceptors.response.use(
-  (response) => response,
-  (error) => {
-    if (error.response?.status === 401) {
-      logout()
-      router.push("/auth/login")
-    }
+// api.interceptors.response.use(
+//   (response) => response,
+//   (error) => {
+//     if (error.response?.status === 401) {
+//       logout()
+//       router.push("/auth/login")
+//     }
 
-    return Promise.reject(error);
-  }
-);
+//     return Promise.reject(error);
+//   }
+// );
 
 export default api;
